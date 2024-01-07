d711f07c584b08f887d4367b3818b1891397973a
Phillip Schichtel <phillip@schich.tel>
2024-01-07 17:48:09 +0100

    either static secret or users

--- charts/coturn/src/entrypoint.sh
+++ charts/coturn/src/entrypoint.sh
@@ -7,17 +7,18 @@ config="/tmp/turnserver.conf"
 # shellcheck disable=SC2005
 echo "$(cat /config/base.conf)" > "$config"
 
-for file in /static-users/*
-do
-    if [ -e "$file" ]
-    then
-        echo "user=$(basename "$file"):$(cat "$file")" >> "$config"
-    fi
-done
 
 if [ -e /static-secret ]
 then
     echo "static-auth-secret=$(cat "/static-secret")" >> "$config"
+else
+    for file in /static-users/*
+    do
+        if [ -e "$file" ]
+        then
+            echo "user=$(basename "$file"):$(cat "$file")" >> "$config"
+        fi
+    done
 fi
 
 # shellcheck disable=SC2005
