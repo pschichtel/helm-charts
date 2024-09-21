fee411d8ff10aaeec915038a49d800bd7027b5dd
Phillip Schichtel <phillip@schich.tel>
2024-09-21 17:05:05 +0200

    upgrade the keycloak operator to 25.0.6

--- charts/keycloak-operator/update.sh
+++ charts/keycloak-operator/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="25.0.5"
+version="25.0.6"
 
 curl -sLfo templates/kubernetes.yml "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/kubernetes.yml"
 
