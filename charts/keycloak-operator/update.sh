04bc0e420959cab1c2cfdefca208d4ca2bb7c689
Phillip Schichtel <phillip@schich.tel>
2025-05-01 21:02:18 +0200

    update keycloak-operator to 26.2.2
    this also automates some parts of the process

--- charts/keycloak-operator/update.sh
+++ charts/keycloak-operator/update.sh
@@ -5,5 +5,16 @@ version="$(yq .appVersion Chart.yaml)"
 cd templates
 rm -v *
 
-curl -sLOf "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/kubernetes.yml"
+file='kubernetes.yml'
 
+remove_noise() {
+    yq -i "del(${1:-}.metadata.labels[\"app.kubernetes.io/managed-by\"]) | del(${1:-}.metadata.annotations[\"app.quarkus.io/build-timestamp\"])" "$file"
+}
+
+curl -sLf -o "$file" "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/kubernetes.yml"
+remove_noise
+remove_noise ".spec.template"
+sed -i -r 's/ClusterRole(Binding)?/Role\1/g' "$file"
+sed -i -r "s/$version/{{ .Release.AppVersion }}/g" "$file"
+sed -i -r 's/image:.+/image: {{ print .Values.image.repository ":" (.Values.image.tag | default .Chart.AppVersion) | quote }}/g' "$file"
+sed -i -r 's/imagePullPolicy:.+/imagePullPolicy: {{ .Values.image.pullPolicy }}/g' "$file"
