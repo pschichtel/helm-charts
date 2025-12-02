a111debea74901362b4da8adbc071ac15fbbeec2
Phillip Schichtel <phillip.schichtel@vier.ai>
2025-12-02 18:15:16 +0100

    soo close, yet so far

--- charts/rabbitmq-topology-operator/update.sh
+++ charts/rabbitmq-topology-operator/update.sh
@@ -70,7 +70,7 @@ do
     fi
     if [ "$kind" = 'validatingwebhookconfiguration' ]
     then
-        yq -i '.metadata.annotations["cert-manager.io/inject-ca-from"] = "{{ print .Release.Namespace }}/" + strenv(RESOURCE_FULLNAME) + "-issuer"' "$f"
+        yq -i '.metadata.annotations["cert-manager.io/inject-ca-from"] = "{{ print .Release.Namespace }}/" + strenv(RESOURCE_FULLNAME) + "-webhook-cert"' "$f"
         yq -i '.webhooks |= map(.clientConfig.service.name = strenv(RESOURCE_FULLNAME) + "-webhook" | .clientConfig.service.namespace = "{{ .Release.Namespace }}")' "$f"
     fi
 done
