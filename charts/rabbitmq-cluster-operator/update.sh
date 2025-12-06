b60e295ec877d2db13354d06141c4decbddb6bb2
Phillip Schichtel <phillip@schich.tel>
2025-12-06 18:17:14 +0100

    move keycloak over to a script similar to rabbitmq

--- charts/rabbitmq-cluster-operator/update.sh
+++ charts/rabbitmq-cluster-operator/update.sh
@@ -18,38 +18,41 @@ rm -v namespace_*.yml
 
 export RESOURCE_NAME='{{ include "rabbitmq-cluster-operator.name" $ }}'
 export RESOURCE_FULLNAME='{{ include "rabbitmq-cluster-operator.fullname" $ }}'
+export NAME_PREFIX='^rabbitmq-cluster(-operator)?'
+export IMAGE='{{ print .Values.image.registry "/" .Values.image.repository ":" (.Values.image.tag | default .Chart.AppVersion) }}'
+
 for f in *.yml
 do
     kind="$(yq '.kind | downcase' "$f")"
     echo "Processing kind: $kind"
     yq -i 'del(.metadata.namespace)' "$f"
-    yq -i '.metadata.name |= sub("^rabbitmq-cluster(-operator)?", strenv(RESOURCE_FULLNAME))' "$f"
+    yq -i '.metadata.name |= sub(strenv(NAME_PREFIX), strenv(RESOURCE_FULLNAME))' "$f"
     yq -i '.metadata.labels["app.kubernetes.io/name"]     = strenv(RESOURCE_NAME)' "$f"
     yq -i '.metadata.labels["app.kubernetes.io/instance"] = "{{ .Release.Name }}"' "$f"
     if [ "$kind" = 'clusterrolebinding' ] || [ "$kind" = "rolebinding" ]
     then
-        yq -i '.roleRef.name |= sub("^rabbitmq-cluster(-operator)?", strenv(RESOURCE_FULLNAME)) | .subjects = (.subjects | map(.namespace = "{{ .Release.Namespace }}" | .name |= sub("^rabbitmq-cluster(-operator)?", strenv(RESOURCE_FULLNAME))))' "$f"
+        yq -i '.roleRef.name |= sub(strenv(NAME_PREFIX), strenv(RESOURCE_FULLNAME)) | .subjects = (.subjects | map(.namespace = "{{ .Release.Namespace }}" | .name |= sub(strenv(NAME_PREFIX), strenv(RESOURCE_FULLNAME))))' "$f"
     fi
     if [ "$kind" = 'deployment' ]
     then
         yq -i '.spec.selector.matchLabels["app.kubernetes.io/name"]         = strenv(RESOURCE_NAME)' "$f"
         yq -i '.spec.selector.matchLabels["app.kubernetes.io/instance"]     = "{{ .Release.Name }}"' "$f"
-	yq -i '.spec.template.metadata.labels["app.kubernetes.io/name"]     = strenv(RESOURCE_NAME)' "$f"
+        yq -i '.spec.template.metadata.labels["app.kubernetes.io/name"]     = strenv(RESOURCE_NAME)' "$f"
         yq -i '.spec.template.metadata.labels["app.kubernetes.io/instance"] = "{{ .Release.Name }}"' "$f"
-	yq -i '.spec.template.spec.serviceAccountName                      |= sub("^rabbitmq-cluster(-operator)?", strenv(RESOURCE_FULLNAME))' "$f"
-	yq -i '.spec.template.spec.containers[0].image                      = "{{ print .Values.image.registry \"/\" .Values.image.repository \":\" (.Values.image.tag | default .Chart.AppVersion) }}"' "$f"
-	yq -i '.spec.template.spec.containers[0].imagePullPolicy            = "{{ .Values.image.pullPolicy }}"' "$f"
-	yq -i '.spec.template.spec.containers[0].imagePullSecrets           = "with12:{{ .Values.image.imagePullSecrets }}"' "$f"
-	yq -i '.spec.template.spec.containers[0].resources                  = "{{- .Values.resources | toYaml | nindent 12 }}"' "$f"
-	yq -i '.spec.replicas                                               = "{{ .Values.replicaCount }}"' "$f"
-	yq -i '.spec.template.spec.nodeSelector                             = "with8:{{ .Values.nodeSelector }}"' "$f"
-	yq -i '.spec.template.spec.tolerations                              = "with8:{{ .Values.toleratios }}"' "$f"
-	yq -i '.spec.template.spec.affinity                                 = "with8:{{ .Values.affinity }}"' "$f"
-	yq -i '.spec.template.metadata.annotations                          = "with8:{{ .Values.podAnnotations }}"' "$f"
-	# remove unneeded quoting
-	sed -i -r "s/'\\{\\{(.+)}}'/{{\\1}}/g" "$f"
-	# introduce with blocks
-	sed -i -r -r "s/(\\s+)([^:]+):\s+with([0-9]+):\\{\\{\\s*(.+?)}}/\\1{{- with \\4 }}\\n\\1\\2: {{- toYaml . | nindent \\3 }}\\n\\1{{- end }}/g" "$f"
+        yq -i '.spec.template.spec.serviceAccountName                      |= sub(strenv(NAME_PREFIX), strenv(RESOURCE_FULLNAME))' "$f"
+        yq -i '.spec.template.spec.containers[0].image                      = strenv(IMAGE)' "$f"
+        yq -i '.spec.template.spec.containers[0].imagePullPolicy            = "{{ .Values.image.pullPolicy }}"' "$f"
+        yq -i '.spec.template.spec.containers[0].imagePullSecrets           = "with12:{{ .Values.image.imagePullSecrets }}"' "$f"
+        yq -i '.spec.template.spec.containers[0].resources                  = "{{- .Values.resources | toYaml | nindent 12 }}"' "$f"
+        yq -i '.spec.replicas                                               = "{{ .Values.replicaCount }}"' "$f"
+        yq -i '.spec.template.spec.nodeSelector                             = "with8:{{ .Values.nodeSelector }}"' "$f"
+        yq -i '.spec.template.spec.tolerations                              = "with8:{{ .Values.toleratios }}"' "$f"
+        yq -i '.spec.template.spec.affinity                                 = "with8:{{ .Values.affinity }}"' "$f"
+        yq -i '.spec.template.metadata.annotations                          = "with8:{{ .Values.podAnnotations }}"' "$f"
+        # remove unneeded quoting
+        sed -i -r "s/'\\{\\{(.+)}}'/{{\\1}}/g" "$f"
+        # introduce with blocks
+        sed -i -r -r "s/(\\s+)([^:]+):\s+with([0-9]+):\\{\\{\\s*(.+?)}}/\\1{{- with \\4 }}\\n\\1\\2: {{- toYaml . | nindent \\3 }}\\n\\1{{- end }}/g" "$f"
     fi
 done
 
