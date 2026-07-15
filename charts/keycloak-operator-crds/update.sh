a06040f5ccee2622c311eddef8a262f03778626d
Phillip Schichtel <phillip.schichtel@vier.ai>
2026-07-15 14:15:57 +0200

    add missing CRDs to keycloak

--- charts/keycloak-operator-crds/update.sh
+++ charts/keycloak-operator-crds/update.sh
@@ -5,6 +5,15 @@ version="$(yq .appVersion Chart.yaml)"
 cd templates
 rm -v *
 
-curl -sLOf "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml"
-curl -sLOf "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml"
+files=(
+  keycloakoidcclients.k8s.keycloak.org-v1.yml
+  keycloakrealmimports.k8s.keycloak.org-v1.yml
+  keycloaks.k8s.keycloak.org-v1.yml
+  keycloaksamlclients.k8s.keycloak.org-v1.yml
+)
+
+for file in "${files[@]}"
+do
+  curl -sLOf "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${version}/kubernetes/$file"
+done
 
